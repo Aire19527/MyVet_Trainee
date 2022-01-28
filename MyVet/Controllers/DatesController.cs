2658afcfc8d043a984e6d86969400ad3632ea916
Juan Pablo Rodriguez Toro <44555206+Aire19527@users.noreply.github.com>
2022-01-28 14:37:23 -0500

    Controlador de citas

---
+++ MyVet/Controllers/DatesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyVet.Controllers
+{
+    public class DatesController : Controller
+    {
+        // GET: DatesController
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
